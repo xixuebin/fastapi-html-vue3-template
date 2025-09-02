e7e9aecf88af873e071802c23ec0fca464a4aa07
kevin.xi <kevinxi@aspendigital.co>
2025-09-02 10:48:09 +0800

    feat:init project

--- src/com/offer/ops/controller/hello_controller.py
+++ src/com/offer/ops/controller/hello_controller.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
+from starlette import status
+from starlette.responses import RedirectResponse
 
 router = APIRouter()
 
 @router.get("/", operation_id="hello_world")
 async def root():
-    return {"message": "Hello World"}
+    return RedirectResponse(url="/index.html", status_code=status.HTTP_302_FOUND)
 
 @router.get("/hello/{name}", operation_id="hello_name")
 async def say_hello(name: str):
